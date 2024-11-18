be387f6bcad8f264d7d4f2c51d24593bab78b61a
SHENZA <shenza@oocl.com>
2024-11-18 21:12:18 +0800

    [MultiplicationTable] test, add test

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -54,4 +54,17 @@ class MultiplicationTableBuilderTest {
         //then
         assertNull(multiplicationTable);
     }
+
+    @Test
+    void should_return_multiply_table_when_build_given_start_number_2_end_number_5() {
+        //given
+        MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
+        int start = 2;
+        int end = 5;
+        //when
+        String multiplicationTable = builder.build(start, end);
+        String expectedMultiplicationTable = String.format("2*2=4%n2*3=6 3*3=9%n2*4=8 3*4=12 4*4=16%n2*5=10 3*5=15 4*5=20 5*5=25%n");
+        //then
+        assertEquals(expectedMultiplicationTable, multiplicationTable);
+    }
 }
